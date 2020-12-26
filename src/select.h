a13fec5509b5c01f4bd9616be1c0fc4846dc829d
MichaelDim02 <31562759+MichaelDim02@users.noreply.github.com>
2020-12-26 14:38:58 +0000

    Update select.h

--- src/select.h
+++ src/select.h
@@ -1,3 +1,14 @@
-/*int counter = 0;*/
+/*
+ * HOUNDSNIFF by Michael C. Dim. (Kerberos)
+ *        hash identification program
+ *
+ *        select.h
+ */
+
+#ifndef SELECT_H
+#define SELECT_H
+
 int callback(void *, int, char **, char **);
 int sel(int length, const char *charset);
+
+#endif
