6fd663ec539ddbb1e9b3ad90e90d404e2ef16dfe
MichaelDim02 <31562759+MichaelDim02@users.noreply.github.com>
2020-12-26 14:40:06 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -11,6 +11,24 @@
  * by Michael Constantine Dimopoulos (Kerberos) 
  */
  
+/* has uppercase letters */
+bool
+hasUpper(char ch[600])
+{
+	bool hup = false;
+
+	int len = strlen(ch);
+	int i;
+	
+	for (i=0; i<len; i++) {
+		if (ch[i] >= 'A' && ch[i] <= 'Z') {
+			hup = true;
+			break;
+		}  /*else if (ch[i] >= 'a' && ch[i] <= 'z') {*/
+	} 
+	return hup;
+}
+
 void
 banner(float vesion)
 { 
